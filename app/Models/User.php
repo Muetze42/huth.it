1b72e065bf28a5b8a985835b30edb7b35e01d2cb
Norman Huth <norman@huth.it>
2021-09-08 20:15:00 +0200

    Fixed Google Token Refresh Method

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Log;
 
 class User extends Authenticatable
 {
