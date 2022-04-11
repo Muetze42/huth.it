494cd30b23a2fe761679239cd23af7bc558a0123
Norman Huth <norman@huth.it>
2022-04-11 22:09:15 +0200

    Add fonts

--- config/filesystems.php
+++ config/filesystems.php
@@ -71,7 +71,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
-        public_path('assets') => storage_path('app/assets'),
+//        public_path('assets') => storage_path('app/assets'),
     ],
 
 ];
