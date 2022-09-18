accb29ce44c693d471983184129e7ca03d86eac6
Norman Huth <norman@huth.it>
2022-09-18 11:03:55 +0200

    Prevent vertical scrolling in paragraphs

--- resources/js/app/Pages/Privacy/Index.vue
+++ resources/js/app/Pages/Privacy/Index.vue
@@ -3,8 +3,8 @@
         <button type="button" class="btn" lang="en" :class="{ 'active': lang == 'en' }" @click="lang = 'en'">English</button>
         <button type="button" class="btn" lang="de" :class="{ 'active': lang == 'de' }" @click="lang = 'de'">Deutsch</button>
     </div>
-    <div lang="en" v-html="en" v-if="lang == 'en'" />
-    <div lang="de" v-html="de" v-else />
+    <div lang="en" v-html="en" v-if="lang == 'en'" class="privacy-text" />
+    <div lang="de" v-html="de" v-else class="privacy-text" />
 </template>
 
 <script>
