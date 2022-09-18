8f28f3749fd5090a7857b490be760ac260e35457
Norman Huth <norman@huth.it>
2022-09-18 17:03:35 +0200

    Wording

--- resources/js/app/Pages/Home/Index.vue
+++ resources/js/app/Pages/Home/Index.vue
@@ -5,7 +5,7 @@
     <p>Nice that you found your way to my site.</p>
     <p>If you want to know more about me or what I do, I invite you to use one of my links in the footer of the page.</p>
     <hr>
-    <p>My page is available in light and dark mode. You can change these modes in the header of the page.</p>
+    <p>My site is available in light and dark mode. You can change these modes in the header of the page.</p>
     <p>In the menu you will find a few links and tools.</p>
 </template>
 
