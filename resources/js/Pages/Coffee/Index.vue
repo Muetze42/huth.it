20a4a1b1a50abcaab1bbc0c0beb2a4ffa6f50e33
Norman Huth <norman@huth.it>
2023-02-10 03:09:47 +0100

    Update affiliate links

--- resources/js/Pages/Coffee/Index.vue
+++ resources/js/Pages/Coffee/Index.vue
@@ -27,6 +27,12 @@
     </div>
     <h3 class="my-8">Advertising links</h3>
     <div class="coffee">
+        <div>
+            <a href="https://uptimerobot.com/?rid=0db9c0c413f465" target="_blank"  class="no-hover">
+                <img src="/assets/uptimerobot-logo.svg" alt="UptimeRobot - The world's leading uptime monitoring service." class="w-96">
+                <span class="block">The world's leading <span style="color: #3bd671">uptime monitoring</span> service.</span>
+            </a>
+        </div>
 <!--        <div>-->
 <!--            <a href="https://www.netcup.de/?ref=177959" target="_blank" class="no-hover">-->
 <!--                <img src="https://www.netcup.de/static/assets/images/promotion/netcup-setC-234x60.png" alt="Netcup GmbH">-->
@@ -34,6 +40,7 @@
 <!--        </div>-->
         <div>
             <a href="https://all-inkl.com/PA77D721D085F2D" target="_blank" class="no-hover">
+                <span class="sr-only">ALL-INKL.COM - Webhosting Server Hosting Domain Provider</span>
                 <img src="/assets/all-inkl.jpg" alt="ALL-INKL.COM - Webhosting Server Hosting Domain Provider">
             </a>
         </div>
@@ -44,7 +51,80 @@
         </div>
         <div>
             <a href="https://de.igraal.com/einladung?werber=AG_5a1ab979a2ff2" target="_blank" class="no-hover">
+                <span class="sr-only">iGraal Cashback</span>
                 <img src="https://st-de-filebanking.igstatic.com/front/banner/28.gif" alt="iGraal Cashback">
+            </a>
+        </div>
+        <div>
+            <a href="https://www.digistore24.com/redir/174033/NormanHuth/CAMPAIGNKEY" target="_blank" class="no-hover">
+                <span class="flex justify-between space-x-2 items-center max-w-md mx-auto">
+                    <span>
+                        <img src="/assets/erecht24/erecht24-logo-premium-horizontal-white.png" alt="eRecht24 Logo" class="h-8">
+                    </span>
+                    <span class="grow text-left">
+                        eRecht24 Premium <span class="font-semibold italic">Agenturen</span> ABO Standard
+                    </span>
+                </span>
+            </a>
+        </div>
+        <div>
+            <a href="https://www.digistore24.com/redir/174029/NormanHuth/CAMPAIGNKEY" target="_blank" class="no-hover">
+                <span class="flex justify-between space-x-2 items-center max-w-md mx-auto">
+                    <span>
+                        <img src="/assets/erecht24/erecht24-logo-premium-horizontal-white.png" alt="eRecht24 Logo" class="h-8">
+                    </span>
+                    <span class="grow text-left">
+                        eRecht24 Premium <span class="font-semibold italic">Shops</span> ABO Standard
+                    </span>
+                </span>
+            </a>
+        </div>
+        <div>
+            <a href="https://www.digistore24.com/redir/174027/NormanHuth/CAMPAIGNKEY" target="_blank" class="no-hover">
+                <span class="flex justify-between space-x-2 items-center max-w-md mx-auto">
+                    <span>
+                        <img src="/assets/erecht24/erecht24-logo-premium-horizontal-white.png" alt="eRecht24 Logo" class="h-8">
+                    </span>
+                    <span class="grow text-left">
+                        eRecht24 Premium <span class="font-semibold italic">Webseiten</span> ABO Standard
+                    </span>
+                </span>
+            </a>
+        </div>
+        <div>
+            <a href="https://www.digistore24.com/redir/287335/NormanHuth/CAMPAIGNKEY" target="_blank" class="no-hover">
+                <span class="flex justify-between space-x-2 items-center max-w-md mx-auto">
+                    <span>
+                        <img src="/assets/erecht24/erecht24-logo-premium-horizontal-white.png" alt="eRecht24 Logo" class="h-8">
+                    </span>
+                    <span class="grow text-left">
+                        eRecht24 Premium <span class="font-semibold italic">Shops</span> Netto-Preise
+                    </span>
+                </span>
+            </a>
+        </div>
+        <div>
+            <a href="https://www.digistore24.com/redir/287336/NormanHuth/CAMPAIGNKEY" target="_blank" class="no-hover">
+                <span class="flex justify-between space-x-2 items-center max-w-md mx-auto">
+                    <span>
+                        <img src="/assets/erecht24/erecht24-logo-premium-horizontal-white.png" alt="eRecht24 Logo" class="h-8">
+                    </span>
+                    <span class="grow text-left">
+                        eRecht24 Premium <span class="font-semibold italic">Agenturen</span> Netto-Preise
+                    </span>
+                </span>
+            </a>
+        </div>
+        <div>
+            <a href="https://www.digistore24.com/redir/287338/NormanHuth/CAMPAIGNKEY" target="_blank" class="no-hover">
+                <span class="flex justify-between space-x-2 items-center max-w-md mx-auto">
+                    <span>
+                        <img src="/assets/erecht24/erecht24-logo-premium-horizontal-white.png" alt="eRecht24 Logo" class="h-8">
+                    </span>
+                    <span class="grow text-left">
+                        eRecht24 Premium <span class="font-semibold italic">Webseiten</span> Netto-Preise
+                    </span>
+                </span>
             </a>
         </div>
     </div>
