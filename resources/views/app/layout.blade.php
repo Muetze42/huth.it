00b1d7081bd331da27913293898759feeb7a960e
Norman Huth <norman@huth.it>
2023-04-26 10:52:35 +0200

    Move open graph for LinkedIn

--- resources/views/app/layout.blade.php
+++ resources/views/app/layout.blade.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>{{ $pageTitle }}</title>
+    @include('app.og')
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="index,follow"/>
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
     @inertiaHead
     @include('app.favicon')
-    @include('app.og')
 </head>
 <body>
 @inertia
