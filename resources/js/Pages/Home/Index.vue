e27583b09fe499982e53ba7e323255a97b8d6a48
Norman Huth <norman@huth.it>
2023-12-03 09:30:10 +0100

    refactor: update job and add tinkerwell link

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -3,10 +3,12 @@
     <p>Hi, I’m Norman</p>
     <p>born on June 17th and I’m a developer.</p>
     <p>Nice that you found your way to my site.</p>
-    <p>I’m <strong>Backend Core Developer</strong> at <a href="https://reaze.com" target="_blank">reaze</a>.</p>
+    <p>I’m <strong>Developer</strong> at <a href="https://www.linkedin.com/company/kosatec-computer-gmbh/" target="_blank">KOSATEC Computer GmbH</a>.</p>
     <p>If you want to know more about me or what I do, I invite you to use one of my links in the footer of the page.</p>
     <hr>
     <p>I would be glad if you follow me or connect with me on <a href="https://www.linkedin.com/in/normanhuth/" target="_blank"><font-awesome-icon icon="fa-brands fa-linkedin" fixed-width />Linked</a>.</p>
+    <hr>
+    <p>Enjoy coding and debugging Editor <a href="https://tinkerwell.app/ref/Qdxy2Abv19" target="_blank">Tinkerwell</a></p>
 </template>
 
 <script>
